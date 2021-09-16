a8242a1c63e96d6fd4e9f2c35d6a3ca7e9754b6c
Souza <38145379+misael28@users.noreply.github.com>
2021-09-15 23:50:44 -0300

    feat: add font style at projec

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Next</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}></h1>
+    </>
+  );
 }
