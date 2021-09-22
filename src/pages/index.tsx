5bceaae7a83b969f968ce5b54f2b677a5942fe29
Souza <38145379+misael28@users.noreply.github.com>
2021-09-22 01:40:43 -0300

    feat: add submit button at home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -17,10 +18,10 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+		  <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
-      <h1></h1>
     </>
   );
 }
