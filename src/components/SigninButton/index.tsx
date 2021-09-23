f52326161f0bba4cd7ba1e67dc54ec34cf3bbe30
Souza <38145379+misael28@users.noreply.github.com>
2021-09-23 00:32:13 -0300

    fix: correct signin directory

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
-import { signIn, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function SignInButton() {
   const [session] = useSession();
 
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Misael Souza
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
