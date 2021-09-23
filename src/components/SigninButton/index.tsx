87fa5cd8452487cfad6c67f58a4604394aaff102
Souza <38145379+misael28@users.noreply.github.com>
2021-09-23 00:19:52 -0300

    feat: add provider at app

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,17 +1,23 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, useSession } from "next-auth/client";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       Misael Souza
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
