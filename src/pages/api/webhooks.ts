d3dc4d15d63dfa36901619864dd046cd90070eac
Souza <38145379+misael28@users.noreply.github.com>
2021-10-01 20:57:02 -0300

    feat: add stripe webhook at aplication

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunck of readable) {
+    chunks.push(typeof chunck === "string" ? Buffer.from(chunck) : chunck);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set(["checkout.session.completed"]);
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const secret = req.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    const type = event.type;
+
+    if (relevantEvents.has(type)) {
+      console.log("Evento recebido", event);
+    }
+
+    res.json({ received: "true" });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Not allowed");
+  }
+}
