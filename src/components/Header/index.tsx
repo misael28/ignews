6784d77e167c69794876ad0827680a68847ed045
Souza <38145379+misael28@users.noreply.github.com>
2021-10-02 19:01:07 -0300

    feat: create active link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SigninButton";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active}  href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
